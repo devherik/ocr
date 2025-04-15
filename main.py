aaeb863f10dbc06d494d0a2d6b8b899eb749e5ed
Herik Colares Rezende <dev.herik@gmail.com>
2025-04-15 05:45:19 -0300

    logic: mapping working pretty well

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ocr_class
 def main():
     ocr = ocr_class.Ocr()
-    ocr.mapear_tela(ocr.tirar_screenshot('screenshot.png', 0, 0, ocr.resolucao[0], ocr.resolucao[1]))
+    ocr.mapear_tela(10)
     
 if __name__ == "__main__":
     main()
--- ocr_class.py
+++ ocr_class.py
@@ -31,13 +31,13 @@ class Ocr():
 
     def buscar_texto_em_tela(self, texto: str) -> bool:
         print('--- Iniciando busca por palavra ---')
-        self.tirar_screenshot('screenshot.png', 0, 0, 1920, 1080)
+        self.tirar_screenshot('screenshot.png', 0, 0, self.resolucao[0], self.resolucao[1])
         imagem = cv2.imread('screenshot.png')
         imagem_gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
         texto_encontrado = self.__ocr.image_to_string(imagem_gray)
         return True if texto == texto_encontrado else False
 
-    def tirar_screenshot(self, nome_arquivo: str, x1: int, y1: int, x2: int, y2: int) -> str:
+    def tirar_screenshot(self, nome_arquivo: str, x1: int, y1: int, x2: int, y2: int) -> None:
         print('Tirando screenshot')
         try:
             # tira um screenshot partindo dos pontos x1 e y2, somando à eles os valores x2 e y2
@@ -47,29 +47,31 @@ class Ocr():
             return None
         imagem.save(nome_arquivo)
         print(f'Screenshot salva como {nome_arquivo}')
-        return nome_arquivo
 
     def pegar_resolucao(self) -> tuple:
         print('--- Pegando resolução da tela ---')
         return self.__automation.size()
 
-    def mapear_tela(self, imagem_path: str) -> None:
+    def mapear_tela(self, qnt_quadrantes: int) -> None:
         print('--- Mapeando tela ---')
-        mapa = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [
-            1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+        time.sleep(2)
+        mapa = []
+        for r in range(qnt_quadrantes):
+            mapa.append([])
+            for c in range(qnt_quadrantes): mapa[r].append(c)
         # define o tamanho de cada quadrante
-        quadrante_x = self.__width / 5
-        quadrante_y = self.__height / 5
+        quadrante_x = self.__width / qnt_quadrantes
+        quadrante_y = self.__height / qnt_quadrantes
         # percorre a tela e divide em quadrantes
-        for y in range(5):
-            for x in range(5):
+        for y in range(qnt_quadrantes):
+            for x in range(qnt_quadrantes):
                 x1 = int(x * quadrante_x)
                 y1 = int(y * quadrante_y)
                 x2 = int((x + 1) * quadrante_x)
                 y2 = int((y + 1) * quadrante_y)
                 # salva a imagem do quadrante
-                imagem_quadrante = self.tirar_screenshot(f'quadrante_{x}_{y}.png', x1, y1, int(quadrante_x), int(quadrante_y))
+                self.tirar_screenshot(f'quadrante_{x}_{y}.png', x1, y1, int(quadrante_x), int(quadrante_y))
                 # adiciona o quadrante ao mapa
                 mapa[x][y] = (x1, y1, x2, y2)
-                print(f'Quadrante {x}_{y} salvo como {imagem_quadrante}')
+                print(f'Quadrante {x}_{y} salvo como quadrante_{x}_{y}.png')
         print('--- Mapeamento concluído ---')
